0856511cfbed6eb3c0823b6b3ccf183293800f30
Aka deax <patrickqwertz@gmail.com>
2022-05-27 23:04:32 +0200

    added player animation and controls

--- Assets/Scripts/Anthill.cs
+++ Assets/Scripts/Anthill.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnthillController : MonoBehaviour
+public class Anthill : MonoBehaviour
 {
 
     void Start()
